40f9dabf771aeff2122bd692ae0b8e3979f72217
VideoGameGod <97229350+VideoGameGod@users.noreply.github.com>
2025-07-12 15:11:38 +0100

    Delete S-Zomb/compile.bat

--- S-Zomb/compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-g++ -o S-zomb.exe S-zomb.cpp -lgdi32 -luser32 -lwinmm
-if %errorlevel% == 0 (
-    echo Compilation successful!
-    S-zomb.exe
-) else (
-    echo Compilation failed!
-    pause
-)
